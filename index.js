63a68db2d4898292e685638013d51bb44366175f
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-07-28 13:55:30 +0300

    completed lab functions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, searchWord) {
+    return drivers.filter(driver => driver.toLowerCase().includes(searchWord.toLowerCase()))
+}
+
+function fuzzyMatch(drivers, beginLetter) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(beginLetter.toLowerCase()))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
